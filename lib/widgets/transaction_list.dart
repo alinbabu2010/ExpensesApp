db96e0175c0f904cdd4347166a6ac67c296b5d37
Alin Babu <alinbabu2010@gmail.com>
2023-01-02 17:22:22 +0530

    Add listView builder and refactor to separate widget for each transaction card

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,5 +1,5 @@
+import 'package:expenses_app/widgets/transaction_item.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
@@ -12,55 +12,12 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView(
-        children: transactions.map((transaction) {
-          return Card(
-            child: Row(
-              children: [
-                Container(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 15,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.purple,
-                      width: 2,
-                    ),
-                  ),
-                  padding: const EdgeInsets.all(10),
-                  child: Text(
-                    "₹${transaction.amount.toStringAsFixed(2)}",
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      transaction.title,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                    ),
-                    Text(
-                      DateFormat().format(transaction.date),
-                      style: const TextStyle(
-                        color: Colors.grey,
-                        fontSize: 12,
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-          );
-        }).toList(),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          var transaction = transactions[index];
+          return TransactionItem(transaction: transaction);
+        },
+        itemCount: transactions.length,
       ),
     );
   }
