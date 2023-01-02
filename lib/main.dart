4409e9ac091df183effccbf484f4a2b69bbfdc35
Alin Babu <alinbabu2010@gmail.com>
2023-01-02 16:15:18 +0530

    Refactor code to split widgets adding new widgets created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:expenses_app/transaction.dart';
+import 'package:expenses_app/widgets/user_transactions.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,13 +19,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-        id: 't1', title: "New Shoes", amount: 1200, date: DateTime.now()),
-    Transaction(
-        id: 't2', title: "Groceries", amount: 2500, date: DateTime.now()),
-  ];
-
   MyHomePage({Key? key}) : super(key: key);
 
   @override
@@ -36,67 +28,17 @@ class MyHomePage extends StatelessWidget {
         title: const Text("Flutter App"),
       ),
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              width: double.infinity,
-              child: const Card(
-                color: Colors.blueAccent,
-                elevation: 5,
-                child: Text("Chart!"),
-              ),
-            ),
-            Column(
-              children: transactions.map((transaction) {
-                return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        )),
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          "₹${transaction.amount.toStringAsFixed(2)}",
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.purple),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            transaction.title,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Text(
-                            DateFormat().format(transaction.date),
-                            style: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 12,
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            )
-          ]),
+          mainAxisAlignment: MainAxisAlignment.start, children: [
+        Container(
+          width: double.infinity,
+          child: const Card(
+            color: Colors.blueAccent,
+            elevation: 5,
+            child: Text("Chart!"),
+          ),
+        ),
+        const UserTransactions()
+      ]),
     );
   }
 }
