12145700450a719067a6b8e836952719bfc59b29
Alin Babu <alinbabu2010@gmail.com>
2023-01-03 10:20:11 +0530

    Add keyboard input type and input action in NewTransaction widget
    Also extract button submit action to a new function and add validation

--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -8,32 +8,39 @@ class NewTransaction extends StatelessWidget {
   NewTransaction({Key? key, required this.addTransactionHandler})
       : super(key: key);
 
+  /// To submit data to [addTransactionHandler] using text editing controller
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.tryParse(amountController.text) ?? 0.0;
+    if (enteredTitle.isEmpty || enteredAmount <= 0) return;
+    addTransactionHandler(enteredTitle, enteredAmount);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 5,
       child: Container(
-        padding: EdgeInsets.all(10),
+        padding: const EdgeInsets.all(10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             TextField(
               decoration: const InputDecoration(labelText: "Title"),
               controller: titleController,
+              textInputAction: TextInputAction.next,
               // onChanged: (value) => titleInput = value,
             ),
             TextField(
               decoration: const InputDecoration(labelText: "Amount"),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
+              // On iOS use 'const TextInputType.numberWithOptions(decimal: true)'
               // onChanged: (value) => amountInput = value,
             ),
             TextButton(
-              onPressed: () {
-                addTransactionHandler(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-              },
+              onPressed: submitData,
               style: ButtonStyle(
                   foregroundColor: MaterialStateProperty.all(Colors.purple)),
               child: const Text("Add transaction"),
