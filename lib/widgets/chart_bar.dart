c991726ff290d8b5fbb43f0ac19dbb401e741514
Alin Babu <alinbabu2010@gmail.com>
2023-01-03 17:27:14 +0530

    Add sizedBox to wrap fittedBox widget and reverse the groupedTransactionValues list

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -16,7 +16,12 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        FittedBox(child: Text('₹${spendingAmount.toStringAsFixed(0)}')),
+        SizedBox(
+          height: 20,
+          child: FittedBox(
+            child: Text('₹${spendingAmount.toStringAsFixed(0)}'),
+          ),
+        ),
         const SizedBox(height: 4),
         SizedBox(
           height: 60,
