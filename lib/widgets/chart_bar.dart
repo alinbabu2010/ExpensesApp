7e470ebda0a47f9a411868895bfa84f4c62ee2e3
Alin Babu <alinbabu2010@gmail.com>
2023-01-03 16:59:48 +0530

    Add chart bar widget

---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingTotal;
+
+  const ChartBar(
+    this.label,
+    this.spendingAmount,
+    this.spendingTotal, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        FittedBox(child: Text('₹${spendingAmount.toStringAsFixed(0)}')),
+        const SizedBox(height: 4),
+        SizedBox(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                    color: const Color.fromRGBO(220, 220, 220, 1),
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+              FractionallySizedBox(
+                heightFactor: spendingTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        const SizedBox(height: 4),
+        Text(label),
+      ],
+    );
+  }
+}
